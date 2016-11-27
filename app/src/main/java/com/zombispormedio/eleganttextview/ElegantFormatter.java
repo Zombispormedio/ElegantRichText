3bba1ebb3fed31c164abb6071eda786f109c9c03
Xavier Serrano <zombispormedio007@gmail.com>
2016-11-27 17:18:08 +0100

    format elms

--- app/src/main/java/com/zombispormedio/eleganttextview/ElegantFormatter.java
+++ app/src/main/java/com/zombispormedio/eleganttextview/ElegantFormatter.java
@@ -1,7 +1,14 @@
 package com.zombispormedio.eleganttextview;
 
+import android.text.SpannableStringBuilder;
+
+import com.annimon.stream.Stream;
+import com.annimon.stream.function.BiFunction;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 /**
  * Created by xavierserrano on 24/11/16.
@@ -17,7 +24,9 @@ public class ElegantFormatter {
 
     private String endPoint=defaultEndPoint;
 
-    private LinkedHashMap<String,ArrayList<int[]>> positions;
+    private HashMap<String,ArrayList<int[]>> positions;
+
+    private HashMap<String, CharSequence> values;
 
     private String raw;
 
@@ -26,41 +35,156 @@ public class ElegantFormatter {
         this.raw=raw;
         this.startPoint = startPoint;
         this.endPoint = endPoint;
-        this.positions=new LinkedHashMap<>();
+        this.positions=new HashMap<>();
+        this.values=new HashMap<>();
+        resolve();
     }
 
     public ElegantFormatter(String raw) {
         this.raw=raw;
         this.positions=new LinkedHashMap<>();
+        this.values=new HashMap<>();
+        resolve();
     }
 
-    public void setStartPoint(String startPoint) {
+    public ElegantFormatter setPoints(String startPoint, String endPoint) {
         this.startPoint = startPoint;
-    }
-
-    public void setEndPoint(String endPoint) {
         this.endPoint = endPoint;
+        return this;
     }
 
-    public void setRaw(String raw) {
+    public ElegantFormatter setRaw(String raw) {
         this.raw = raw;
-        resolveBindingLocations();
+        return this;
     }
 
-    public void resolveBindingLocations(){
+    public ElegantFormatter resolve(){
         positions.clear();
 
         StringBuilder maker=new StringBuilder(raw);
 
         int startPosition=maker.indexOf(startPoint);
 
+        int offset=0;
+
         while(startPosition>-1){
+            int endPosition=maker.indexOf(endPoint);
+
+            String key=maker.substring(startPosition+1, endPosition).trim();
+
+            int realStartPosition=startPosition+offset;
+            int realEndPosition=endPosition+offset;
+
+            int[] loc=new int[]{realStartPosition, realEndPosition};
+            if(positions.containsKey(key)){
+                positions.get(key).add(loc);
+
+            }else{
+                ArrayList<int[]> keyPositions=new ArrayList<>();
+                keyPositions.add(loc);
+                positions.put(key, keyPositions);
+            }
+
+            maker=new StringBuilder(maker.substring(endPosition+1));
+
+            offset+=endPosition+1;
+
+            startPosition=maker.indexOf(startPoint);
 
         }
 
+
+        return this;
+
     }
 
-    public LinkedHashMap<String, ArrayList<int[]>> getPositions() {
+    public ElegantFormatter put(String key, CharSequence value){
+        values.put(key, value);
+        return this;
+    }
+
+    public HashMap<String, ArrayList<int[]>> getPositions() {
         return positions;
+    }
+
+    public SpannableStringBuilder apply(){
+        SpannableStringBuilder builder=new SpannableStringBuilder(raw);
+
+        return Stream.of(positions.keySet())
+                .reduce(builder, (memo, key) -> {
+                    CharSequence v=values.get(key);
+
+                    return Stream.of(positions.get(key))
+                            .reduce(memo, bindFunction(v));
+                });
+
+    }
+
+
+    private BiFunction<SpannableStringBuilder, int[], SpannableStringBuilder> bindFunction(CharSequence value){
+        return (b, l) -> b.replace(l[0], l[1]+1,value);
+    }
+
+
+    private abstract class Value<V>{
+        private V value;
+
+        public Value(V value) {
+            this.value = value;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+    private class CharSequenceValue extends Value<CharSequence>{
+
+        public CharSequenceValue(CharSequence value) {
+            super(value);
+        }
+    }
+
+    private class ListCharSequenceValue extends Value<ArrayList<CharSequence>>{
+
+        public ListCharSequenceValue(ArrayList<CharSequence> value) {
+            super(value);
+        }
+    }
+
+    public static class Joiner{
+
+        private String betweenElements;
+
+        private String twoElements;
+
+        private String moreThanTwoElements;
+
+        public Joiner(String betweenElements, String twoElements, String moreThanTwoElements) {
+            this.betweenElements = betweenElements;
+            this.twoElements = twoElements;
+            this.moreThanTwoElements = moreThanTwoElements;
+        }
+
+        public SpannableStringBuilder join(List<CharSequence> elems){
+            if(elems.size()>2){
+                return joinMoreThanTwo(elems);
+            }else if(elems.size()==2){
+                return joinTwo(elems);
+
+            }else if(elems.size()==1){
+                return new SpannableStringBuilder(elems.get(0));
+            }else{
+                return new SpannableStringBuilder();
+            }
+        }
+
+        private SpannableStringBuilder joinTwo(List<CharSequence> elems){
+            return new SpannableStringBuilder();
+        }
+
+        private SpannableStringBuilder joinMoreThanTwo(List<CharSequence> elems){
+            return new SpannableStringBuilder();
+        }
     }
 }
--- app/src/test/java/com/zombispormedio/eleganttextview/ElegantFormatterTest.java
+++ app/src/test/java/com/zombispormedio/eleganttextview/ElegantFormatterTest.java
@@ -4,14 +4,80 @@ package com.zombispormedio.eleganttextview;
  * Created by xavierserrano on 26/11/16.
  */
 
-import org.junit.Test;
 
-import org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
 public class ElegantFormatterTest {
+
+    public final String firstString="Hola, {name}, Lorem ipsum dolor sit amet, consectetur adipiscin" +
+            "g elit. Integer eget tortor velit. {name} tincidunt consectetur pharetra. Ut tem" +
+            "por lacinia urna commodo blandit. {hello} ac nibh augue. Sed ornare erat hendrerit posuere porta.";
+
 
     @Test
     public void resolveBindingPositions(){
-        
+
+        final ElegantFormatter formatter=new ElegantFormatter(firstString);
+
+        assertEquals(2, formatter.getPositions().size());
+
+        HashMap<String, ArrayList<int[]>> expectedPositions=mockExpectedPositions();
+
+        HashMap<String, ArrayList<int[]>> testPositions=formatter.getPositions();
+
+        for (String k:expectedPositions.keySet()) {
+
+            assertTrue(testPositions.containsKey(k));
+
+            ArrayList<int[]> expected=expectedPositions.get(k);
+            ArrayList<int[]> test=testPositions.get(k);
+
+            assertEquals(expected.size(), test.size());
+
+            for(int i=0; i< test.size();i++){
+                int [] p=test.get(i);
+                assertEquals(k, firstString.substring(p[0]+1, p[1]));
+            }
+        }
+
+    }
+
+    private HashMap<String, ArrayList<int[]>> mockExpectedPositions(){
+        HashMap<String, ArrayList<int[]>> expectedPositions=new HashMap<>();
+        ArrayList<int[]> namePositions=new ArrayList<>();
+        namePositions.add(new int[]{6, 11});
+        namePositions.add(new int[]{});
+        expectedPositions.put("name", namePositions);
+
+        ArrayList<int[]> helloPositions=new ArrayList<>();
+        helloPositions.add(new int[]{});
+        expectedPositions.put("hello", helloPositions);
+        return expectedPositions;
+    }
+
+    @Test
+    public void testApply(){
+        final ElegantFormatter formatter=new ElegantFormatter(firstString);
+
+        String result=formatter.put("name", "Xavier")
+                .put("hello", "Bienvenido")
+                .apply().toString();
+
+        assertEquals("Hola, Xavier, Lorem ipsum dolor sit amet, consectetur adipiscin" +
+                "g elit. Integer eget tortor velit. Xavier tincidunt consectetur pharetra. Ut tem" +
+                "por lacinia urna commodo blandit. Bienvenido ac nibh augue. Sed ornare erat hendrerit posuere porta.", result);
+
     }
 
 }
